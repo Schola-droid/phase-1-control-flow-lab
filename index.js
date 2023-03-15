b18f2f73c42d7e050409eff8a6ce5c4dbc3fbf6b
Scholastica Nzomo <scholambula1@gmail.com>
2023-03-15 12:34:55 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+  if(feet <= 400){
+    return 'This one is on me!'
+  } else if (2000<feet && feet<2500){
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
+  } 
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log (scuberGreetingForFeet(2600))
+function ternaryCheckCity(city){
+  const destination = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return destination
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log (ternaryCheckCity ('Pittsburgh'))
+function switchOnCharmFromTip(tip){
+  switch (tip) { 
+   case 'generous' :
+    return 'Thank you so much.';
+    break;
+   case'not as generous':
+    return 'Thank you.';
+    break;
+   default :
+    return 'Bye.'
+  }
 }
+console.log (switchOnCharmFromTip('generous'))
